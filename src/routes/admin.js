412f7fb602d1a377a0adbc0ce6f169e4d0c3b009
jaywyawhare <2005441@kiit.ac.in>
2025-04-18 01:40:43 +0530

    Removed generic module

--- src/routes/admin.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-    res.redirect('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
-});
-
-module.exports = router;
